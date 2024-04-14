a52366ec19c9cada51c04e7bfcdcc3a7c5eba665
Marcos Fierro <73545192+MarcosAlferoFierroSarriamfierros@users.noreply.github.com>
2024-04-14 15:51:01 -0500

    Update script3.m

--- script3.m
+++ script3.m
@@ -1,30 +1,21 @@
-% Crear el suscriptor al tópico de pose de Turtle1
-% Crear el suscriptor al tópico de pose de Turtle1
+%Nos suscribimos al tema de turtle1
 poseSubscriber = rossubscriber('/turtle1/pose');
 
-pause(1); % Espera un segundo para asegurarse de recibir el último mensaje
+pause(1); 
+% Mensaje con datos de a pose
+latestPose = receive(poseSubscriber, 1); % Se hace una pausa hasta recibir un mensaje
 
-% Obtener el último mensaje de pose
-latestPose = receive(poseSubscriber, 1); % Espera hasta recibir un mensaje
-
-% Mostrar la posición y orientación de Turtle1
+% Mostrar la posición y orientación de la tortuga
 disp('Última pose de Turtle1:');
 disp(['Posición (x, y): ', num2str(latestPose.X), ', ', num2str(latestPose.Y)]);
 disp(['Orientación (z): ', num2str(latestPose.Theta)]);
-
-% Crear un objeto de cliente de servicio para el servicio de teleportación absoluta
 teleportClient = rossvcclient('/turtle1/teleport_absolute');
-
-% Crear una solicitud de servicio
 req = rosmessage(teleportClient);
 
-% Establecer la nueva posición y orientación de Turtle1
-req.X = 5; % Nueva posición en el eje X
-req.Y = 5; % Nueva posición en el eje Y
-req.Theta = pi/2; % Nueva orientación (en radianes)
-
-% Llamar al servicio de teleportación absoluta para establecer la nueva pose de Turtle1
+% Ponemos una posiciòn random en la tortuga para ver que se mueva
+req.X = 5; 
+req.Y = 5; 
+req.Theta = pi/2; % en rads
 resp = call(teleportClient, req);
-
-% Finalizar el nodo maestro de ROS
+% Finalizamos nodo maestro de ROS
 rosshutdown;
